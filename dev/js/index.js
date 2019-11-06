91c9080447a7290de159f5785741666133e98c3c
pilipalashushushu <a47d5b@gmail.com>
2019-11-06 11:32:47 +0800

    index.js modify

--- dev/js/index.js
+++ dev/js/index.js
@@ -7,6 +7,8 @@ $(function () {
         anchors: ["cyclist", "專屬騎行", "單車體驗", "最新活動", "Cy嚴選", "不可不知", "關於Cy"],
         navigationTooltips: ["cyclist", "專屬騎行", "單車體驗", "最新活動", "Cy嚴選", "不可不知", "關於Cy"],
         showActiveTooltip: true,
+        keyboardScrolling: true,
+        loopBottom: true,
     });
 
 });
