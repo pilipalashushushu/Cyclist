9e37ff41d706947723edc887edefc4ff97eb42ff
chloe1230 <chou100900@gmail.com>
2019-11-15 18:51:14 +0800

    前導畫面(未完成)

--- gulpfile.js
+++ gulpfile.js
@@ -144,7 +144,7 @@ gulp.task('default', function(){
     browserSync.init({
         server: {
             baseDir: "./dest",
-            index: "index.html"
+            index: "welcome.html"
         }
     });
 
