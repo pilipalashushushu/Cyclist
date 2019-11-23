bc755152ff2723f89270b5f3d7616b86f9140dcd
chloe1230 <chou100900@gmail.com>
2019-11-23 15:23:57 +0800

    shoppinCart

--- dev/js/shoppingCart.js
+++ dev/js/shoppingCart.js
@@ -1,60 +1,108 @@
-var incart = [];
+$(function (e) {
+    if (sessionStorage['cart-list'] == null) {
+        //購物車中沒有東西時，顯示'您尚未選購商品'並且總額不顯示
+        $('.cartEmpty').css('display', 'block');
+        $('.finalamount').text(' ');
+    } else {
+        //購物車中有東西時，顯示商品清單
+        $('.cartEmpty').css('display', 'none');
+        $('.detail').css('display', 'flex');
+    }
+});
+
+
+var arr = sessionStorage["cart-list"].slice(0, -1).split(',');
+var length = arr.length - 1;
+// console.log(length);
+var rows = []; //[ [], [], [] ]
+for (var i = 0; i <= length; i++) {
+    var row = sessionStorage[`cart-row-${arr[i]}`].slice(0, -1).split(',');
+    // console.log(row);
+    var obj = {
+        name: row[0],
+        price: row[1],
+        num: parseInt(row[2]),
+        img: row[3],
+    }
+    rows.push(obj);
+}
+// console.log(rows);
+
+
+//Vue渲染
 var app = new Vue({
     el: '#cart',
     data: {
         Psum: 0,
         productList: sessionStorage["cart-list"].slice(0, -1).split(','),
+        rows: rows, // [ {}, {}, {} ]
     },
     methods: {
-        //取得itemList
-        getItems(i) {
-            return sessionStorage[`cart-row-${i}`];
+        //     //增加數量
+        //     handlePlus(cart) {
+        //         this.prodAmount++;
+        //         this.prodPrice += this.prodPrice;
+        //         this.finalamount;
+        //     },
+        //     //減少數量
+        //     handleSub(cart) {
+        //         if (this.prodAmount > 1) {
+        //             this.prodAmount--;
+        //             this.prodSum -= this.prodPrice;
+        //         }
+        //         this.finalamount;
+        //     },
+        //     //刪除商品
+        //     handledelete(index) {
+        //         this.productList.splice(index, 1);
+        //     },
+        upData(){
+           val = document.querySelector('input[type="text"]'); 
+        //    val =  $(this).parent().find('input').val();
+           console.log(val);
         },
-        //取得商品名稱
-        getprodName(i) {
-            cartRow = sessionStorage[`cart-row-${i}`];
-            prodName = cartRow.split(',')[0];
-            return prodName;
-        },
-        //取得商品單價
-        getprodPrice(i) {
-            cartRow = sessionStorage[`cart-row-${i}`];
-            prodPrice = parseInt(cartRow.split(',')[1]);
-            return prodPrice;
-        },
-        //取得商品數量
-        getprodAmount(i) {
-            cartRow = sessionStorage[`cart-row-${i}`];
-            prodAmount = parseInt(cartRow.split(',')[2]);
-            return prodAmount;
-        },
-        //增加數量
-        handlePlus() {
-            this.prodAmount++;
-            this.prodPrice += this.prodPrice;
-            // this.finalamount;
-        },
-        //減少數量
-        handleSub() {
-            if (this.prodAmount > 1) {
-                this.prodAmount--;
-                this.prodSum -= this.prodPrice;
-            }
-            // this.finalamount;
-        },
-        //刪除商品
-        handledelete(index) {
-            this.productList.splice(index, 1);
-        },
+    
     },
     computed: {
         //計算總和
         finalamount() {
-            this.Psum = 0;
+            // this.Psum = 0;
             // for (i in this.productList) {
             //     this.Psum += this.productList[i].prodPrice;
             // }
-            return this.Psum;
+            // return this.Psum;
+            let total = 0;
+            if (sessionStorage['cart-list'] == null) {
+                return total
+            } else {
+                for (var i = 0; i < rows.length; i++) {
+                    total += rows[i]["price"] * rows[i]["num"];
+                }
+                return total
+            }
         },
     }
 })
+
+
+// sessionStorage['final-list'] = 
+
+
+
+
+
+
+
+
+// $(function () {
+//     $.ajax({
+//         url: "shoppingCart.php",
+//         datType: "json",
+//         type: "GET",
+//         success: function (data) {
+//             console.log(data);
+//         }
+//     });
+// });
+
+// window.addEventListener('load', init, false);
