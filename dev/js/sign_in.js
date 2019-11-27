5231437efe97e6ff1c28167414fe1066aff169ad
chloe1230 <chou100900@gmail.com>
2019-11-27 12:02:53 +0800

    同步所有頁面 ‘登入畫面及登入顯示狀態’

--- dev/js/sign_in.js
+++ dev/js/sign_in.js
@@ -18,38 +18,8 @@ $(document).ready(function () {
                     sessionStorage['memNickName'] = meminfo.memNickName;
                     sessionStorage['memPic'] = meminfo.memPic;
                 }
+                window.open('./cyclist.html','_self');
             },
         });
     });
-
-    var memId = sessionStorage.getItem('memId');
-    // alert(sessionStorage.getItem['memId'])
-    if(sessionStorage['member'] != null || sessionStorage['member'] != undefined ){
-        $('.memberId').innerText(memId);
-        $('#login').innerText('登出');
-    }
 })
-
-// $(document).ready(function () {
-//     $('#sign-up-btn').click(function () {
-//         // console.log( $("#memNickName0").val() );
-//         // console.log( $("#memId0").val() );
-//         // console.log( $("#memPsw0").val() );
-//         // console.log( $("#memEmail0").val() );
-
-//         $.ajax({
-//             url: './php/sign_up.php',
-//             type: 'POST',
-//             data: {
-//                 memNickName: $("#memNickName0").val(),
-//                 memId: $("#memId0").val(),
-//                 memPsw: $("#memPsw0").val(),
-//                 memEmail: $("#memEmail0").val(),
-//             },
-//             success(data) {
-//                 alert(data);
-//                 window.open('member.html', '_self');
-//             },
-//         });
-//     });
-// })
